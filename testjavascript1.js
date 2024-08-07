a2c7a008a8dd303af8ac9122b702e51694e05b80
glynda <glyncan123@gmail.com>
2024-08-06 23:00:17 -0700

    testjavascript1.js

--- testjavascript1.js
+++ testjavascript1.js
@@ -1,1 +1,11 @@
-# hello javascript
+console.log("javascript script testing git ");
+
+const greet = function (fname) {
+  console.log(`Hi, ${fname}`);
+};
+greet("Carlos");
+
+const lastName = (lname) => {
+  return `Last Name: ${lname}`;
+};
+console.log(lastName("Yulo"));
